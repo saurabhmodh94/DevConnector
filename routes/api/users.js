294e15d13e903de3b4632aae461aceacac8db52b
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-26 18:46:26 +0530

    gravatar config and API status code

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 // Load User model
 const User = require('../../models/User');
@@ -17,11 +18,18 @@ router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = 'Email already exists';
-      return res.json(errors);
+      return res.status(400).json(errors);
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
+        avatar,
         password: req.body.password
       });
 
