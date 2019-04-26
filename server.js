08d97b6a8b57d86c27bea84126a7dd50a5383865
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-26 17:50:24 +0530

    basic db connection

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+
+// DB Config
+const keys = require('./config/keys');
+
+// Connect to MongoDB
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true // to get rid of warning
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
