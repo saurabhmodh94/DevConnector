5c953fba2acd8fc6315877aa23e262dd7061cef9
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-27 13:37:14 +0530

    basic react-router-dom config and modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,9 +9,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
+        <Router>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </Router>
       </div>
     );
   }
