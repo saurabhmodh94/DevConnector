9aa41aba3308d3022a915490c6d46d67bd330f7b
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-27 19:50:43 +0530

    footer dynamic year

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; 2019 Dev Connector
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
     </footer>
   );
 }
