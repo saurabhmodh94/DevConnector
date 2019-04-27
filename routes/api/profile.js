cfcf6e0ac74ffb53fdbaa7dc8fe05bf5bbd8a9ae
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-27 11:04:00 +0530

    basic delete education and user&profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,7 @@ const validateEducationInput = require('../../validation/education');
 
 // Load Models
 const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
@@ -254,6 +255,45 @@ router.delete(
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
   }
 );
 
