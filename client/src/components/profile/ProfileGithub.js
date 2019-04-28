d0e86d99e2de1f58c8219a9b69f51d408bbf4ac8
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-28 16:14:26 +0530

    basic profileGithub component

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProfileGithub extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      repos: []
+    };
+  }
+  componentDidMount() {
+    const { username } = this.props;
   }
 
   render() {
-    return <div>ProfileGithub</div>;
+    const { repos } = this.state;
+
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>
+              <Link to={repo.html_url} className="text-info" target="_blank">
+                {repo.name}
+              </Link>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <div className="col-md-6">
+            <span className="badge badge-info mr-1">
+              Stars: {repo.stargazers_count}
+            </span>
+            <span className="badge badge-secondary mr-1">
+              Watchers: {repo.watchers_count}
+            </span>
+            <span className="badge badge-success">
+              Forks: {repo.forks_count}
+            </span>
+          </div>
+        </div>
+      </div>
+    ));
+    return (
+      <div>
+        <hr />
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repoItems}
+      </div>
+    );
   }
 }
 
